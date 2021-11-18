42aa0541e7917aed0fdacd61288adea863ac5a2c
Julio Fuentes <juliodddddd1234@gmail.com>
2021-11-18 10:43:37 -0600

    App config is ready

---
+++ src/components/Box.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../app.css'
+
+const index = () => {
+    return (
+        <div className="flex justify-center items-center text-white bg-gray-600 w-12 h-12 rounded-sm">
+            <div className="h-1/2">
+                Hola
+            </div>
+        </div>
+    );
+}
+
+export default index;
